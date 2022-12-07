79dff48d7c129793580e2c77277c29063cbea79b
MarcelloCavazza <marcellocavazzaoliveira@gmail.com>
2022-12-07 06:19:14 -0300

    learning props.children

--- src/component/ExpensesMainContainer.js
+++ src/component/ExpensesMainContainer.js
@@ -1,15 +1,16 @@
+import { Card } from "./Card"
 import { ExpenseItem } from "./ExpenseItem"
 import './ExpensesMainContainer.css'
 export const ExpensesMainContainer = (props) => {
 
     return (<>
-        <div className="expenses">
+        <Card className="expenses">
             {
                 props.data.map((service) => {
                     return <ExpenseItem key={service.id} title={service.title} price={service.price} date={service.date} />
                 })
             }
-        </div>
+        </Card>
     </>)
 
 }
